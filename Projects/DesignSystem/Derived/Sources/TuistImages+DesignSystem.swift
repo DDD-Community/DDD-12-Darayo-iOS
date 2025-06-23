5c24b0b01851ea92fc668c79f9854f72ad6c4335
Jungwon Lee <dev.jungwon.lee@gmail.com>
2025-06-24 01:40:49 +0900

    feat: implement home navigation bar ui

--- Projects/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
+++ Projects/DesignSystem/Derived/Sources/TuistImages+DesignSystem.swift
@@ -7,10 +7,14 @@ public extension Image {
     
     static let iconBell = Self("icon_bell")
     static let iconCalendar = Self("icon_calendar")
+    static let iconCalendarMode = Self("icon_calendar_mode")
+    static let iconChecked = Self("icon_checked")
+    static let iconGridMode = Self("icon_grid_mode")
     static let iconHome = Self("icon_home")
     static let iconList = Self("icon_list")
     static let iconMyPage = Self("icon_my_page")
     static let iconPicture = Self("icon_picture")
     static let iconPlus = Self("icon_plus")
     static let iconTimetable = Self("icon_timetable")
+    static let iconUnchecked = Self("icon_unchecked")
 }
--- Projects/Features/Home/Sources/HomeFeature.swift
+++ Projects/Features/Home/Sources/HomeFeature.swift
@@ -10,18 +10,31 @@ import ComposableArchitecture
 
 @Reducer
 public struct HomeFeature {
+    @ObservableState
     public struct State {
+        var displayMode: DisplayMode = .grid
+        var isFiltered: Bool = false
+        
         public init() {}
     }
     
-    public enum Action {
-        
+    public enum Action: BindableAction {
+        case binding(BindingAction<State>)
     }
     
     public init() {}
     public var body: some ReducerOf<Self> {
+        BindingReducer()
+        
         Reduce { state, action in
             return .none
         }
+    }
+}
+
+extension HomeFeature {
+    enum DisplayMode {
+        case grid
+        case calendar
     }
 }
