f764a5fef4b83a0c9c3da9384e9425100a8bf95a
Jungwon Lee <dev.jungwon.lee@gmail.com>
2025-07-06 00:36:57 +0900

    feat: implement my tab navigation

---
+++ Projects/Features/MyPage/Sources/PrivacyPolicy/PrivacyPolicyFeature.swift
@@ -0,0 +1,28 @@
+//
+//  PrivacyPolicyFeature.swift
+//  MyPage
+//
+//  Created by 이정원 on 7/6/25.
+//  Copyright © 2025 Darayo. All rights reserved.
+//
+
+import ComposableArchitecture
+
+@Reducer
+public struct PrivacyPolicyFeature {
+    @ObservableState
+    public struct State {
+        public init() {}
+    }
+    
+    public enum Action {
+        
+    }
+    
+    public init() {}
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            return .none
+        }
+    }
+}
---
+++ Projects/Features/MyPage/Sources/TermsOfService/TermsOfServiceFeature.swift
@@ -0,0 +1,28 @@
+//
+//  TermsOfServiceFeature.swift
+//  MyPage
+//
+//  Created by 이정원 on 7/6/25.
+//  Copyright © 2025 Darayo. All rights reserved.
+//
+
+import ComposableArchitecture
+
+@Reducer
+public struct TermsOfServiceFeature {
+    @ObservableState
+    public struct State {
+        public init() {}
+    }
+    
+    public enum Action {
+        
+    }
+    
+    public init() {}
+    public var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            return .none
+        }
+    }
+}
